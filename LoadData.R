19f4c4c02cf9d323c7f74a9b861fc6a1165c0a18
Amaury Rolin <arolin@gmail.com>
2015-05-01 13:59:10 +0700

    Split into preID and HEF groups with new variable names

--- LoadData.R
+++ LoadData.R
@@ -1,8 +1,11 @@
 lpsraw<-read.csv("lps.csv");
 lps=lpsraw;
 
+ColNames<-read.csv("Constants/ColNames.csv");
+names(lps)<-ColNames$VarName;
+
 #Fix Gender
-lps$"q1_4"<-factor(lps$"q1_4",levels = c("1","2"),labels=c("F","M"));
+lps$Gender<-factor(lps$Gender,levels = c("1","2"),labels=c("F","M"));
 
 PossesionNames = c("Video","Mobile","Camera","Television","Bicycle","Motorbike","Car or truck","Tractor","Boat","Motor Boat","N cattle","N buffalo","N horses","N pigs","N goats","N chickens");
 DistNames = c("Lamam","Kaleum","Dukjeung","Thateang")
@@ -20,18 +23,18 @@ PreId<-lps$id.format==1;
 r=1;
 for (i in 1:length(PreId)) {
   vrow <-which (Villages$VillageNum == lps$Village[r]);
-  PreId[r]<-Villages$PreId[vrow];
+  PreId[r]<-!Villages$PreId[vrow];
   r<-r+1;
 }
 lps<-cbind (lps,PreId)
 
 
-lps$"q1_5"<-factor(lps$"q1_5",c("1","3"),c("Yes","No"));
+lps$VilPoor<-factor(lps$VilPoor,c("1","3"),c("Yes","No"));
 
-HEFCard <-!is.na(lps$"q1_8_6");
-GVTPoor <-!is.na(lps$"q1_8_7");
-VilPoor <-lps$"q1_5"=="Yes";
-
+HEFCard <-!is.na(lps$HH_Insurance_HEFCard);
+GVTPoor <-!is.na(lps$HH_Insurance_GVTPoor);
+NoInsu <- !is.na(lps$HH_Insurance_Informal_no_insuranc)
+VilPoor <-lps$VilPoor=="Yes";
 
 #Split Dependents
 #source("SplitDeps.R")
